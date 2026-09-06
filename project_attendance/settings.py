e6f14ff0eb090c7c4bf654e2c330e0eaf1227658
Oto Okino <108208574+oto-okino@users.noreply.github.com>
2026-09-06 18:36:29 +0900

    Fix CSRF_TRUSTED_ORIGINS assignment in settings

--- project_attendance/settings.py
+++ project_attendance/settings.py
@@ -26,7 +26,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
-CSRF_TRUSTED_ORIGINS = os.getenv('CSRF_TRUSTED_ORIGINS')
+CSRF_TRUSTED_ORIGINS = [os.getenv('CSRF_TRUSTED_ORIGINS')]
 
 # Application definition
 
