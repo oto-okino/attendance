4e68060490b378e64eafdf02cecc19953019dc3f
Oto Okino <108208574+oto-okino@users.noreply.github.com>
2026-09-14 00:29:47 +0900

    Update settings.py

--- project_attendance/settings.py
+++ project_attendance/settings.py
@@ -127,6 +127,8 @@ USE_TZ = True
 STATIC_URL = "static/"
 STATICFILES_DIRS = [BASE_DIR / "static"]
 
+STATIC_ROOT = BASE_DIR / "staticfiles"
+
 STORAGES = {
     "default": {
         "BACKEND": "django.core.files.storage.FileSystemStorage",
