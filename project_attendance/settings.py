d828071f51e676360728079d76c5556e67a7be84
Oto Okino <108208574+oto-okino@users.noreply.github.com>
2026-09-13 23:27:58 +0900

    Configure static file storage settings
    Add storage configuration for static files using Whitenoise.

--- project_attendance/settings.py
+++ project_attendance/settings.py
@@ -124,6 +124,14 @@ USE_TZ = True
 STATIC_URL = "static/"
 STATICFILES_DIRS = [BASE_DIR / "static"]
 
+STORAGES = {
+    "default": {
+        "BACKEND": "django.core.files.storage.FileSystemStorage",
+    },
+    "staticfiles": {
+        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+    },
+}
 
 # Email
 # https://docs.djangoproject.com/en/6.1/topics/email/#topic-email-configuration
