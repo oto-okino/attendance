7cb2e41a3661b4b30a4ef3e86f8249167cb80fbc
Oto Okino <take.day.ama.8738@gmail.com>
2026-09-24 00:46:06 +0900

    edit organizations class name

--- organizations/models.py
+++ organizations/models.py
@@ -8,7 +8,7 @@ class Tenant(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
-class Workspace(models.Model):
+class Workplace(models.Model):
     name = models.CharField(max_length=255)
     tenant = models.ForeignKey(Tenant, on_delete=models.CASCADE, related_name='workspaces')
     created_at = models.DateTimeField(auto_now_add=True)
