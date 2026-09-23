49e25f0bc741ada74d5e1e10a06007feee3498bd
Oto Okino <take.day.ama.8738@gmail.com>
2026-09-24 00:44:23 +0900

    create organizations

---
+++ organizations/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+from django.conf import settings
+
+# Create your models here.
+class Tenant(models.Model):
+    name = models.CharField(max_length=255)
+    domain = models.CharField(max_length=255, unique=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+class Workspace(models.Model):
+    name = models.CharField(max_length=255)
+    tenant = models.ForeignKey(Tenant, on_delete=models.CASCADE, related_name='workspaces')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
--- project_attendance/settings.py
+++ project_attendance/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
     'accounts',
     'dashboard',
     'attendance',
+    'organizations'
 ]
 
 MIDDLEWARE = [
